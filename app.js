de01b2f1305f0e475ad6fabb7a733511c4f35ff7
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2023-09-17 16:07:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+const createGallery = function(){
+    for (let i = 1; i <= 500; i++) {
+        const pokeDiv = document.createElement('div');
+        const pokeCard = document.createElement('img');
+        const pokeLabel = document.createElement('span');
+        pokeDiv.classList.add('pokemon');
+        pokeCard.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+        pokeLabel.innerText = `#${i}`;
+        pokeDiv.appendChild(pokeCard);
+        pokeDiv.appendChild(pokeLabel);
+        cont.append(pokeDiv);
+        button1.type = 'hidden';
+
+    }
+}
+
